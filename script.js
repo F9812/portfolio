94dba75523085085bfe9cfd87ba80562456f53fe
F9812 <uakreper1209@gmail.com>
2025-11-22 03:47:57 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.getElementById("button1").addEventListener("click", function() {
-    document.getElementById("message").textContent = "You clicked the first button!";
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    document.getElementById("message").textContent = "You clicked the second button!";
-});
